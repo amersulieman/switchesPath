35b3b94ae9a5c78332041a204f65d88b7d45a1cd
amersulieman <aksuliem@uno.edu>
2019-04-07 20:57:31 -0500

    sending packets in progress, port number weird

--- SpanningTree.py
+++ SpanningTree.py
@@ -29,12 +29,29 @@ def switchesConnection(problemNum,listOFproblems,switches):
         switches[int(a)].switchesConnectedTo.append(int(b))
         switches[int(b)].switchesConnectedTo.append(int(a))
 
+def sending(switches):
+    for switch in switches.values():
+        for physicalConnection in switch.switchesConnectedTo:
+            port = switch.switchesConnectedTo.index(physicalConnection)+1
+            packet =[switch.id,switch.root,switch.numOFhubs,port]
+            switches[physicalConnection].packetsReceived.append(tuple(packet))
 
 myswitches = createSwitches(1,fileInput)
 switchesConnection(1,fileInput,myswitches)
+sending(myswitches)
 
-print(1,myswitches[1].switchesConnectedTo)
-print(2,myswitches[2].switchesConnectedTo)
-print(3,myswitches[3].switchesConnectedTo)
-print(4,myswitches[4].switchesConnectedTo)
-print(5,myswitches[5].switchesConnectedTo)
+
+
+
+
+
+
+
+
+
+
+print(1,myswitches[1].packetsReceived)
+print(2,myswitches[2].packetsReceived)
+print(3,myswitches[3].packetsReceived)
+print(4,myswitches[4].packetsReceived)
+print(5,myswitches[5].packetsReceived)

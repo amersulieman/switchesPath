880b4089eddf1a8e9f8ca852f25dcf3652de7c7f
amersulieman <aksuliem@uno.edu>
2019-04-08 22:56:15 -0500

    sending and receiving working with port dealt with before sending

--- SpanningTree.py
+++ SpanningTree.py
@@ -1,5 +1,4 @@
 class Switch():
-
     def __init__(self,id):
         self.id=id
         self.root=id
@@ -26,32 +25,44 @@ def switchesConnection(problemNum,listOFproblems,switches):
     for connection in connectionSetup:
         switchesConnections.append(tuple(connection.split("-")))
     for a,b in switchesConnections: 
-        switches[int(a)].switchesConnectedTo.append(int(b))
-        switches[int(b)].switchesConnectedTo.append(int(a))
+        if(switches[int(a)].switchesConnectedTo.count(int(b))>0):
+            switches[int(a)].switchesConnectedTo.append(-1)
+            switches[int(b)].switchesConnectedTo.append(-1)
+        else:
+            switches[int(a)].switchesConnectedTo.append(int(b))
+            switches[int(b)].switchesConnectedTo.append(int(a))
 
 def sending(switches):
     for switch in switches.values():
         for physicalConnection in switch.switchesConnectedTo:
+            if physicalConnection ==-1:
+                continue
             port = switch.switchesConnectedTo.index(physicalConnection)+1
             packet =[switch.id,switch.root,switch.numOFhubs,port]
             switches[physicalConnection].packetsReceived.append(tuple(packet))
 
+def receiving(switches):
+    for switch in switches.values():
+        for (a,b,c,d) in switch.packetsReceived:
+            if b < switch.root:
+                print("updating ",switch.id)
+                switch.root = b
+                switch.numOFhubs = c+1
+            switch.packetsReceived.remove((a,b,c,d))
+
 myswitches = createSwitches(1,fileInput)
 switchesConnection(1,fileInput,myswitches)
-sending(myswitches)
+sending(myswitches) 
+print(1,myswitches[1].root,myswitches[1].numOFhubs)
+print(2,myswitches[2].root,myswitches[2].numOFhubs)
+print(3,myswitches[3].root,myswitches[3].numOFhubs)
+print(4,myswitches[4].root,myswitches[4].numOFhubs)
+print(5,myswitches[5].root,myswitches[5].numOFhubs)
 
-
-
-
-
-
-
-
-
-
-
-print(1,myswitches[1].packetsReceived)
-print(2,myswitches[2].packetsReceived)
-print(3,myswitches[3].packetsReceived)
-print(4,myswitches[4].packetsReceived)
-print(5,myswitches[5].packetsReceived)
+receiving(myswitches)
+print("After first round receivng")
+print(1,myswitches[1].root,myswitches[1].numOFhubs)
+print(2,myswitches[2].root,myswitches[2].numOFhubs)
+print(3,myswitches[3].root,myswitches[3].numOFhubs)
+print(4,myswitches[4].root,myswitches[4].numOFhubs)
+print(5,myswitches[5].root,myswitches[5].numOFhubs)

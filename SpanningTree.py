89db710cb2c2f804929228bd76f9cb79a6f2b176
amersulieman <aksuliem@uno.edu>
2019-04-09 22:09:41 -0500

    duplicate ports been solved and switches replace the higher port with -1 as if its being dropped

--- SpanningTree.py
+++ SpanningTree.py
@@ -25,17 +25,28 @@ def switchesConnection(problemNum,listOFproblems,switches):
     for connection in connectionSetup:
         switchesConnections.append(tuple(connection.split("-")))
     for a,b in switchesConnections: 
-        switches[int(a)].switchesConnectedTo.append(int(b))
-        switches[int(b)].switchesConnectedTo.append(int(a))
+        switches[int(a)].switchesConnectedTo.append(switches[int(b)])
+        switches[int(b)].switchesConnectedTo.append(switches[int(a)])
 
 def sending(switches):
     for switch in switches.values():
-        #for physicalConnection in switch.switchesConnectedTo:
         for x in range(0,len(switch.switchesConnectedTo)): 
             port = x+1
             packet =[switch.id,switch.root,switch.numOFhubs,port]
-            switches[switch.switchesConnectedTo[x]].packetsReceived.append(tuple(packet))
+            switches[switch.switchesConnectedTo[x].id].packetsReceived.append(tuple(packet))
 
+def validateOnePortPerTwoSwitchConnections(switches):
+    for switch in switches.values():
+        packetsSources =[a for a,b,c,d in switch.packetsReceived]
+        sourcesWithMultiplePacketsSent = set([x for x in packetsSources if packetsSources.count(x)>1])
+        for x in sourcesWithMultiplePacketsSent:
+            portsOfTheseSources = [i for i,z in enumerate(switch.switchesConnectedTo) if z.id == x]
+            for port in portsOfTheseSources[1:]:
+                switch.switchesConnectedTo[port]=-1
+
+
+
+        
 def receiving(switches):
     for switch in switches.values():
         for (a,b,c,d) in switch.packetsReceived:
@@ -48,13 +59,24 @@ def receiving(switches):
 myswitches = createSwitches(1,fileInput)
 switchesConnection(1,fileInput,myswitches)
 sending(myswitches) 
-
 print(1,myswitches[1].packetsReceived)
 print(2,myswitches[2].packetsReceived)
 print(3,myswitches[3].packetsReceived)
 print(4,myswitches[4].packetsReceived)
 print(5,myswitches[5].packetsReceived)
+print(1,myswitches[1].switchesConnectedTo)
+print(2,myswitches[2].switchesConnectedTo)
+print(3,myswitches[3].switchesConnectedTo)
+print(4,myswitches[4].switchesConnectedTo)
+print(5,myswitches[5].switchesConnectedTo)
 
+validateOnePortPerTwoSwitchConnections(myswitches)
+print("after")
+print(1,myswitches[1].switchesConnectedTo)
+print(2,myswitches[2].switchesConnectedTo)
+print(3,myswitches[3].switchesConnectedTo)
+print(4,myswitches[4].switchesConnectedTo)
+print(5,myswitches[5].switchesConnectedTo)
 
 '''
 print(1,myswitches[1].root,myswitches[1].numOFhubs)

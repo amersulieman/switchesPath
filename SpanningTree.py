5003955a4bd2a3ecac486359d7487c9e9192a3a7
amersulieman <aksuliem@uno.edu>
2019-04-13 23:18:39 -0500

    optimized parsing and connecting between switches. stopped after fixing switchesConnections been fixed

--- SpanningTree.py
+++ SpanningTree.py
@@ -2,62 +2,76 @@ class Switch():
     def __init__(self,id):
         self.id=id
         self.root=id
-        self.switchesConnectedTo =list()
-        self.numOFhubs =0
-        self.packetsReceived =list()
+        self.neighbors =list()
+        self.hubsToRoot =0
+        self.packetsBuffer =list()
 
-
-fileInput = list()
+switchesTests = list()
 with open("input.txt") as myFile:
     for line in myFile:
-        fileInput.append(line)
+        switchesTests.append(line)
 
-def createSwitches(problemLine,problemsList):
-    listOFSwitches = dict()
-    numberOFswitches = int(problemsList[problemLine][0])
-    for i in range(1,numberOFswitches+1):
-        listOFSwitches[i]=Switch(i)
-    return listOFSwitches
+def eachTestComponent(testNumber,testsList):
+    myTest = testsList[testNumber]
+    numOFswitches = int(myTest[0])
+    if myTest[2] == "R":
+        #connectionList = randomTestBuilder()
+        pass
+    else:
+        connectionList = myTest[2:].split()
+        connectionList = [tuple( map(int,pair.split("-")) ) for pair  in connectionList]
+    return numOFswitches,connectionList
 
-def switchesConnection(problemNum,listOFproblems,switches):
-    connectionSetup = listOFproblems[problemNum][2:].split()
-    switchesConnections = list()
-    for connection in connectionSetup:
-        switchesConnections.append(tuple(connection.split("-")))
-    for a,b in switchesConnections: 
-        switches[int(a)].switchesConnectedTo.append(switches[int(b)])
-        switches[int(b)].switchesConnectedTo.append(switches[int(a)])
+def randomTestBuilder():
+    pass
+
+
+def buildSwitches(amount):
+    switchObjects= dict()
+    for i in range(1,amount+1):
+        switchObjects[i]=Switch(i)
+    return switchObjects   
+
+def switchesConnection(switches,connectionsToBeMade):
+    for leftSwitch,rightSwitch in connectionsToBeMade:
+        switchA = switches[leftSwitch]
+        switchB = switches[rightSwitch] 
+        switchA.neighbors.append(switchB)
+        switchB.neighbors.append(switchA)
 
 def sending(switches):
     for switch in switches.values():
-        for x in range(0,len(switch.switchesConnectedTo)):
+        for x in range(0,len(switch.neighbors)):
             port = x+1
-            packet =[switch.id,switch.root,switch.numOFhubs,port]
-            if switch.switchesConnectedTo[x]==-1:
+            packet =[switch.id,switch.root,switch.hubsToRoot,port]
+            if switch.neighbors[x]==-1:
                 continue
             else:
-                switches[switch.switchesConnectedTo[x].id].packetsReceived.append(tuple(packet))
+                switches[switch.neighbors[x].id].packetsBuffer.append(tuple(packet))
 
 def receiving(switches):
     for switch in switches.values():
         l10 =list()
         counter=0
-        packets = switch.packetsReceived[:]
+        packets = switch.packetsBuffer[:]
         for (a,b,c,d) in packets:
             if a in l10:
-                switch.switchesConnectedTo[counter]=-1
-                switches[a].switchesConnectedTo[counter-1]=-1
-                del(switches[a].packetsReceived[d-1])
+                switch.neighbors[counter]=-1
+                switches[a].neighbors[counter-1]=-1
+                del(switches[a].packetsBuffer[d-1])
             else:
                 l10.append(a)
                 if b < switch.root:
                     print("updating ",switch.id)
                     switch.root = b
-                    switch.numOFhubs = c+1
+                    switch.hubsToRoot = c+1
             counter+=1
-            switch.packetsReceived.remove((a,b,c,d))
-myswitches = createSwitches(2,fileInput)
-switchesConnection(2,fileInput,myswitches)
+            switch.packetsBuffer.remove((a,b,c,d))
+
+numberOFswitches,connectionsbetweenSwitches = eachTestComponent(1,switchesTests)
+myswitches = buildSwitches(numberOFswitches)
+switchesConnection(myswitches,connectionsbetweenSwitches)
+'''switchesConnection(2,switchesTests,myswitches)
 sameRootAll =False
 while(sameRootAll==False):
     print("in loop ")
@@ -71,14 +85,14 @@ while(sameRootAll==False):
         print("Done")
 
 for x in myswitches.values():
-    print(x.id,x.root,x.numOFhubs)
+    print(x.id,x.root,x.hubsToRoot)
     
 
 
 
-'''for switch in myswitches.values():
+for switch in myswitches.values():
     l20 =list()
-    for switch2 in switch.switchesConnectedTo:
+    for switch2 in switch.neighbors:
         if switch2 == -1:
             l20.append(switch2)
         else:
